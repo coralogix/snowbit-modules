539ca22bc8341c251c21f7344f7a3a3774d8eb03
Ilan Pogrebinsky <38400762+ilanpogr@users.noreply.github.com>
2023-05-28 16:26:36 +0300

    align terraform variables for azure (#6)

--- STA/azure/sta_ng.tf
+++ STA/azure/sta_ng.tf
@@ -8,19 +8,25 @@ variable "Prefix" {}
 variable "DiskSize" {}
 variable "Region" {}
 variable "STA_Version" {}
+variable "storageAccountResourceGroup" {}
+variable "storageAccount" {}
+variable "storageContainer" {}
 
 
 module "STA" {
   source = "https://coralogix-integrations.s3-eu-west-1.amazonaws.com/cloud-security/terraform-azure/snowbit-sta.template.tgz"
 
-  Region                  = var.Region
-  PrivateKey              = var.PrivateKey
-  CompanyID               = var.CompanyID
-  ApplicationName         = var.ApplicationName
-  CoralogixEndpoint       = var.CoralogixEndpoint
-  CoralogixSyslogEndpoint = var.CoralogixSyslogEndpoint
-  SSH_PublicKey           = file(var.SSHKey)
-  DiskSize                = var.DiskSize
-  Prefix                  = var.Prefix
-  STA_Version              = var.STA_Version
+  Region                        = var.Region
+  PrivateKey                    = var.PrivateKey
+  CompanyID                     = var.CompanyID
+  ApplicationName               = var.ApplicationName
+  CoralogixEndpoint             = var.CoralogixEndpoint
+  CoralogixSyslogEndpoint       = var.CoralogixSyslogEndpoint
+  SSH_PublicKey                 = file(var.SSHKey)
+  DiskSize                      = var.DiskSize
+  Prefix                        = var.Prefix
+  STA_Version                   = var.STA_Version
+  storageAccountResourceGroup   = var.storageAccountResourceGroup
+  storageAccount                = var.storageAccount
+  storageContainer              = var.storageContainer
 }
