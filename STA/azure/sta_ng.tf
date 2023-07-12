ce6400ee510231099fe702751ef267eb1d82e710
Nir Limor <nir.limor@coralogix.com>
2023-07-12 20:53:56 +0300

    update after the complete terraform deployment refactor

--- STA/azure/sta_ng.tf
+++ STA/azure/sta_ng.tf
@@ -1,13 +1,15 @@
 variable "PrivateKey" {}
 variable "CompanyID" {}
 variable "ApplicationName" {}
-variable "CoralogixEndpoint" {}
-variable "CoralogixSyslogEndpoint" {}
+variable "Endpoint" {}
+variable "Azure-KeyManager" {}
+variable "SSHKeyResourceGroup" {}
+variable "Azure-SSHKeyResourceGroup" {}
 variable "SSHKey" {}
 variable "Prefix" {}
 variable "DiskSize" {}
-variable "Region" {}
-variable "STA_Version" {}
+variable "Azure-Region" {}
+variable "STA-Version" {}
 variable "storageAccountResourceGroup" {}
 variable "storageAccount" {}
 variable "storageContainer" {}
@@ -16,17 +18,24 @@ variable "storageContainer" {}
 module "STA" {
   source = "https://coralogix-integrations.s3-eu-west-1.amazonaws.com/cloud-security/terraform-azure/snowbit-sta.template.tgz"
 
-  Region                        = var.Region
-  PrivateKey                    = var.PrivateKey
-  CompanyID                     = var.CompanyID
-  ApplicationName               = var.ApplicationName
-  CoralogixEndpoint             = var.CoralogixEndpoint
-  CoralogixSyslogEndpoint       = var.CoralogixSyslogEndpoint
-  SSH_PublicKey                 = file(var.SSHKey)
-  DiskSize                      = var.DiskSize
-  Prefix                        = var.Prefix
-  STA_Version                   = var.STA_Version
-  storageAccountResourceGroup   = var.storageAccountResourceGroup
-  storageAccount                = var.storageAccount
-  storageContainer              = var.storageContainer
+  Azure-Region                      = var.Azure-Region
+  Coralogix-PrivateKey              = var.PrivateKey
+  Coralogix-CompanyID               = var.CompanyID
+  Coralogix-ApplicationName         = var.ApplicationName
+  Coralogix-Endpoint                = var.Endpoint
+  Azure-SSHKeyName                  = file(var.SSHKey)
+  Azure-KeyManager                  = var.Azure-KeyManager
+  Azure-SSHKeyResourceGroup         = var.Azure-SSHKeyResourceGroup
+  Azure-DiskSize                          = var.DiskSize
+  Azure-Prefix                            = var.Prefix
+  STA-Version                       = var.STA-Version
+  Azure-StorageAccountResourceGroup = var.storageAccountResourceGroup
+  Azure-StorageAccount              = var.storageAccount
+  Azure-StorageContainer            = var.storageContainer
+}
+output "General" {
+  value = module.STA.General
+}
+output "Azure" {
+  value = module.STA.Instance_and_Permissions
 }
